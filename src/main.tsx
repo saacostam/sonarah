6a84d7eba8103214b6213fa0ea4134a83067ae71
saacostam <73088662+saacostam@users.noreply.github.com>
2025-10-13 01:11:51 -0500

    chore(radix-ui): add component library

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
+import { AdaptersProvider } from "@/features/adapters/ui";
 import App from "./App";
-import { AdaptersProvider } from "./features/adapters/ui";
+import "./index.css";
 
 // biome-ignore lint/style/noNonNullAssertion: Default react app initialization
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<AdaptersProvider>
-			<App />
+			<Theme accentColor="crimson">
+				<App />
+			</Theme>
 		</AdaptersProvider>
 	</StrictMode>,
 );
