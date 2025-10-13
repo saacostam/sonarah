6bb9b1c8fb4c07aaa804905869adebaa87e36a67
saacostam <73088662+saacostam@users.noreply.github.com>
2025-10-13 01:18:09 -0500

    chore(tanstack-query): add tanstack-query as async state management

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,24 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@radix-ui/themes/styles.css";
 import { Theme } from "@radix-ui/themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AdaptersProvider } from "@/features/adapters/ui";
 import App from "./App";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 // biome-ignore lint/style/noNonNullAssertion: Default react app initialization
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<AdaptersProvider>
-			<Theme accentColor="crimson">
-				<App />
-			</Theme>
-		</AdaptersProvider>
+		<QueryClientProvider client={queryClient}>
+			<AdaptersProvider>
+				<Theme accentColor="crimson">
+					<App />
+				</Theme>
+			</AdaptersProvider>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	</StrictMode>,
 );
