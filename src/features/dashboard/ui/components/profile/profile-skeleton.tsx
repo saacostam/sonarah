4b1e3f60d6daad0ec9173741d8c6af22e25aa10b
saacostam <73088662+saacostam@users.noreply.github.com>
2025-10-18 00:37:37 -0500

    feat(profile): better match skeleton to real content

--- src/features/dashboard/ui/components/profile/profile-skeleton.tsx
+++ src/features/dashboard/ui/components/profile/profile-skeleton.tsx
@@ -5,7 +5,8 @@ export function ProfileSkeleton() {
 		<Flex gap="4" align="center">
 			<Skeleton height="48px" width="48px" style={{ borderRadius: "100%" }} />
 			<Flex direction="column" gap="2" flexGrow="1">
-				<Skeleton height="24px" width="128px" />
+				<Skeleton height="16px" width="128px" />
+				<Skeleton height="12px" width="128px" />
 			</Flex>
 		</Flex>
 	);
