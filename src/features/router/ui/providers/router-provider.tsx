62a27b7cbc743ca4ce5d23f1f2133b3801424f9e
saacostam <73088662+saacostam@users.noreply.github.com>
2025-10-21 21:50:44 -0500

    feat(router): lazy-load router screens

--- src/features/router/ui/providers/router-provider.tsx
+++ src/features/router/ui/providers/router-provider.tsx
@@ -1,45 +1,69 @@
+import { lazy, Suspense } from "react";
 import { Outlet, Route, Routes } from "react-router";
 import { useAdapters } from "@/features/adapters/app";
 import { AppLayout } from "@/features/app-shell/ui";
-import { DashboardScreen } from "@/features/dashboard/ui";
-import { ErrorScreen } from "@/features/errors/ui";
-import { HomeScreen } from "@/features/home/ui";
-import { ManagePlaylistScreen } from "@/features/playlists/app";
+import { AuthGuardSkeleton } from "@/features/auth/ui";
 import { RouterContext } from "../../app";
 import { RouteName } from "../../domain";
+
+// Lazy imports
+const ManagePlaylistScreen = lazy(() =>
+	import("@/features/playlists/app/screens/manage-playlist-screen").then(
+		(m) => ({ default: m.ManagePlaylistScreen }),
+	),
+);
+const HomeScreen = lazy(() =>
+	import("@/features/home/ui/components/home-screen").then((m) => ({
+		default: m.HomeScreen,
+	})),
+);
+const ErrorScreen = lazy(() =>
+	import("@/features/errors/ui/components/error-screen").then((m) => ({
+		default: m.ErrorScreen,
+	})),
+);
+const DashboardScreen = lazy(() =>
+	import("@/features/dashboard/ui/screens/dashboard-screen").then((m) => ({
+		default: m.DashboardScreen,
+	})),
+);
 
 export function RouterProvider() {
 	const { routerAdapter } = useAdapters();
 
 	return (
 		<RouterContext.Provider value={routerAdapter}>
-			<Routes>
-				<Route
-					element={
-						<AppLayout>
-							<Outlet />
-						</AppLayout>
-					}
-				>
-					<Route index element={<HomeScreen />} />
+			<Suspense fallback={<AuthGuardSkeleton />}>
+				<Routes>
 					<Route
-						path={routerAdapter.defineRoute(RouteName.DASHBOARD)}
-						element={<DashboardScreen />}
-					/>
-					<Route
-						path={routerAdapter.defineRoute(RouteName.PLAYLIST_BY_ID)}
-						element={<ManagePlaylistScreen />}
-					/>
-				</Route>
-				<Route
-					path="*"
-					element={
-						<ErrorScreen
-							resetHref={routerAdapter.generateRoute({ name: RouteName.HOME })}
+						element={
+							<AppLayout>
+								<Outlet />
+							</AppLayout>
+						}
+					>
+						<Route index element={<HomeScreen />} />
+						<Route
+							path={routerAdapter.defineRoute(RouteName.DASHBOARD)}
+							element={<DashboardScreen />}
 						/>
-					}
-				/>
-			</Routes>
+						<Route
+							path={routerAdapter.defineRoute(RouteName.PLAYLIST_BY_ID)}
+							element={<ManagePlaylistScreen />}
+						/>
+					</Route>
+					<Route
+						path="*"
+						element={
+							<ErrorScreen
+								resetHref={routerAdapter.generateRoute({
+									name: RouteName.HOME,
+								})}
+							/>
+						}
+					/>
+				</Routes>
+			</Suspense>
 		</RouterContext.Provider>
 	);
 }
