db4a97a6853ef8baff4b451a88cad240155da455
saacostam <73088662+saacostam@users.noreply.github.com>
2025-11-01 16:40:22 -0500

    refactor: remove notifications context

--- src/features/notifications/app/contexts/notifications.context.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-import type { INotificationAdapter } from "../../domain";
-
-export const NotificationsContext = createContext(
-	null as unknown as INotificationAdapter,
-);
--- src/features/notifications/app/contexts/use-notifications.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { NotificationsContext } from "./notifications.context";
-
-export function useNotifications() {
-	return useContext(NotificationsContext);
-}
