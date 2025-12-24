f9797f4edda06251cf0332d2571e66d16b6f41b8
saacostam <73088662+saacostam@users.noreply.github.com>
2025-12-23 23:06:22 -0500

    test(unfollow-playlist): increase test coverage
    Add unit tests for the unfollow playlist component

---
+++ src/features/playlists/app/components/unfollow-playlist/unfollow-playlist.unit.test.tsx
@@ -0,0 +1,121 @@
+import { screen, waitFor } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
+import { DomainError, DomainErrorType } from "@/shared/adapters/errors/domain";
+import { renderWithProviders } from "@/tests";
+import { UnfollowPlaylist } from "./unfollow-playlist";
+
+describe("UnfollowPlaylist [Unit]", () => {
+	const onCancel = vi.fn();
+	const onSuccess = vi.fn();
+
+	beforeEach(() => {
+		onCancel.mockReset();
+		onSuccess.mockReset();
+	});
+
+	it("should call onCancel when cancel button is clicked", async () => {
+		renderWithProviders(
+			// biome-ignore lint/correctness/useUniqueElementIds: Unit Test
+			<UnfollowPlaylist
+				id="test-id"
+				onCancel={onCancel}
+				onSuccess={onSuccess}
+			/>,
+			{
+				adapters: {
+					notificationsAdapter: {},
+				},
+				repositories: {
+					playlist: {},
+				},
+			},
+		);
+
+		const cancelButton = screen.getByRole("button", { name: /cancel/i });
+		await userEvent.click(cancelButton);
+
+		expect(onCancel).toHaveBeenCalledOnce();
+		expect(onSuccess).not.toHaveBeenCalled();
+	});
+
+	it("should successfully unfollow playlist", async () => {
+		const unfollow = vi.fn();
+
+		renderWithProviders(
+			// biome-ignore lint/correctness/useUniqueElementIds: Unit Test
+			<UnfollowPlaylist
+				id="test-id"
+				onCancel={onCancel}
+				onSuccess={onSuccess}
+			/>,
+			{
+				adapters: {
+					notificationsAdapter: {},
+				},
+				repositories: {
+					playlist: {
+						unfollow,
+					},
+				},
+			},
+		);
+
+		unfollow.mockResolvedValueOnce({ id: "test-id" });
+
+		const unfollowButton = screen.getByRole("button", { name: /unfollow/i });
+		await userEvent.click(unfollowButton);
+
+		await waitFor(() => {
+			expect(unfollow).toHaveBeenCalledOnce();
+			expect(unfollow).toHaveBeenCalledWith({ id: "test-id" });
+		});
+
+		await waitFor(() => {
+			expect(onSuccess).toHaveBeenCalled();
+		});
+	});
+
+	it("should handle errors when unfollowing playlist", async () => {
+		const unfollow = vi.fn();
+		const notify = vi.fn();
+
+		renderWithProviders(
+			// biome-ignore lint/correctness/useUniqueElementIds: Unit Test
+			<UnfollowPlaylist
+				id="test-id"
+				onCancel={onCancel}
+				onSuccess={onSuccess}
+			/>,
+			{
+				adapters: {
+					notificationsAdapter: {
+						notify,
+					},
+				},
+				repositories: {
+					playlist: {
+						unfollow,
+					},
+				},
+			},
+		);
+
+		unfollow.mockRejectedValue(
+			new DomainError(DomainErrorType.BAD_REQUEST, "Error"),
+		);
+
+		const unfollowButton = screen.getByRole("button", { name: /unfollow/i });
+		await userEvent.click(unfollowButton);
+
+		await waitFor(() => {
+			expect(unfollow).toHaveBeenCalledOnce();
+			expect(unfollow).toHaveBeenCalledWith({ id: "test-id" });
+		});
+
+		await waitFor(() => {
+			expect(onCancel).toHaveBeenCalled();
+			expect(notify).toHaveBeenCalled();
+			expect(onSuccess).not.toHaveBeenCalled();
+		});
+	});
+});
