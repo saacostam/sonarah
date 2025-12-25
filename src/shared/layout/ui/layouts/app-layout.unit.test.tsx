b9d95889b20392aaca80c2d876f968168eb353e4
saacostam <73088662+saacostam@users.noreply.github.com>
2025-12-25 15:59:43 -0500

    test(app-layout): increase test coverage
    Add a unit tests for validating the use of the wave background css
    classes. It does test an implementation detail, which isn't ideal, but
    it provides healthy friction to ensure changes are intentional

---
+++ src/shared/layout/ui/layouts/app-layout.unit.test.tsx
@@ -0,0 +1,27 @@
+import { renderWithProviders } from "@/tests";
+import { AppLayout } from "./app-layout";
+
+describe("AppLayout [Unit]", () => {
+	it("should include the css classes necessary to render the background correctly", () => {
+		/**
+		 * The intent of this test is to act as a guard for the CSS class implementation.
+		 * It does test an implementation detail, which isn’t ideal, but it provides useful
+		 * friction to ensure changes are intentional. If the implementation changes, it’s
+		 * okay to update or remove this test accordingly, if behavior is validated.
+		 */
+		const { container } = renderWithProviders(<AppLayout />, {
+			adapters: {
+				navigationAdapter: {
+					generateRoute: vi.fn(),
+				},
+				themeAdapter: {},
+			},
+		});
+
+		const elWithAppShellClass = container.querySelector(".app-shell");
+		expect(elWithAppShellClass).toBeDefined();
+		const elWithAppShellBgClass =
+			elWithAppShellClass?.querySelector("app-shell__bg");
+		expect(elWithAppShellBgClass);
+	});
+});
