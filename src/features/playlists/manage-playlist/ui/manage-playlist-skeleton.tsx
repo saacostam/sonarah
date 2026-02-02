814c326488a5f891cf55f23039d84756e7ef226c
saacostam <73088662+saacostam@users.noreply.github.com>
2026-02-02 15:57:50 -0500

    feat(playlist): update loading skeletons
    - Update loading skeletons to match content

--- src/features/playlists/manage-playlist/ui/manage-playlist-skeleton.tsx
+++ src/features/playlists/manage-playlist/ui/manage-playlist-skeleton.tsx
@@ -1,20 +1,33 @@
 import { Flex, Skeleton } from "@radix-ui/themes";
+import { useMemo } from "react";
+
+const N = 5;
 
 export function ManagePlaylistSkeleton() {
+	const items = useMemo(
+		() =>
+			new Array(N)
+				.fill(null)
+				.map((_, index) => (
+					<Skeleton key={+index} height="72px" width="100%" />
+				)),
+		[],
+	);
+
 	return (
-		<Flex direction="column" gap="4">
+		<Flex data-testid="manage-playlist-skeleton" direction="column" gap="4">
 			<Flex gap="4" justify="between">
 				<Skeleton height="32px" width="175px" />
 				<Skeleton height="32px" width="200px" />
 			</Flex>
-			<Skeleton height="106px" width="100%" />
-			<Skeleton height="24px" width="128px" mt="4" />
+			<Skeleton height="32px" width="160px" />
+			<Skeleton height="102px" width="100%" />
+			<Flex direction="column" gap="2" mt="4">
+				<Skeleton height="24px" width="128px" />
+				<Skeleton height="16px" width="256px" />
+			</Flex>
 			<Flex direction="column" gap="2">
-				<Skeleton height="72px" width="100%" />
-				<Skeleton height="72px" width="100%" />
-				<Skeleton height="72px" width="100%" />
-				<Skeleton height="72px" width="100%" />
-				<Skeleton height="72px" width="100%" />
+				{items}
 			</Flex>
 		</Flex>
 	);
--- src/features/playlists/match-playlist/ui/match-playlist-skeleton.tsx
+++ src/features/playlists/match-playlist/ui/match-playlist-skeleton.tsx
@@ -1,10 +1,28 @@
 import { Flex, Skeleton } from "@radix-ui/themes";
+import { useMemo } from "react";
+
+const N = 5;
 
 export function MatchPlaylistSkeleton() {
+	const items = useMemo(
+		() =>
+			new Array(N)
+				.fill(null)
+				.map((_, index) => (
+					<Skeleton key={+index} height="64px" width="100%" />
+				)),
+		[],
+	);
+
 	return (
-		<Flex direction="column" gap="4">
-			<Skeleton height="24px" width="128px" mb="3" />
-			<Skeleton height="106px" width="100%" />
+		<Flex data-testid="match-playlist-skeleton" direction="column" gap="4">
+			<Skeleton height="32px" width="128px" mb="1" />
+			<Skeleton height="24px" width="128px" mb="1" />
+			<Skeleton height="100px" width="100%" mb="5" />
+			<Skeleton height="24px" width="128px" mb="1" />
+			<Flex direction="column" gap="2">
+				{items}
+			</Flex>
 		</Flex>
 	);
 }
