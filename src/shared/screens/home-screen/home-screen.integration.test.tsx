3b12ae30a13279bf953904212ab8232edc57f59a
saacostam <73088662+saacostam@users.noreply.github.com>
2026-02-08 22:32:24 -0500

    test(home-screen): clean-up di-factory util
    - Clean-up di-factory util function by adding the method mocks within
      the util, and consuming them on test; as opposed, to overriding the
    default output

--- src/shared/screens/home-screen/home-screen.integration.test.tsx
+++ src/shared/screens/home-screen/home-screen.integration.test.tsx
@@ -9,115 +9,101 @@ import { HomeScreen } from "./home-screen";
 
 const navigationAdapter = new NavigationAdapter();
 
-const diFactory = (args: {
-	token: "auth" | "unauth";
-	code?: string;
-}): Parameters<typeof renderWithProviders>["1"] => {
-	return {
+const diFactory = (args: { token: "auth" | "unauth"; code?: string }) => {
+	// adapters
+	const analyticsTrackEvent = vi.fn();
+	const setToken = vi.fn();
+	const startAuthFlow = vi.fn();
+	const resetRouter = vi.fn();
+
+	// repositories
+	const requestAccessToken = vi.fn();
+
+	const di: Parameters<typeof renderWithProviders>[1] = {
 		adapters: {
 			authAdapter: {
 				getToken: () =>
 					args.token === "auth"
-						? {
-								type: "authenticated",
-								token: "token",
-							}
-						: {
-								type: "unauthenticated",
-							},
+						? { type: "authenticated", token: "token" }
+						: { type: "unauthenticated" },
+				setToken,
+				startAuthFlow,
 			},
 			routerAdapter: {
 				getUrlSearchParams: () =>
 					new URLSearchParams({ code: args.code ?? "" }),
+				reset: resetRouter,
 			},
 			navigationAdapter,
+			analyticsAdapter: {
+				trackEvent: analyticsTrackEvent,
+			},
 		},
 		repositories: {
-			auth: {},
+			auth: {
+				requestAccessToken,
+			},
+		},
+	};
+
+	return {
+		di,
+		deps: {
+			analyticsTrackEvent,
+			requestAccessToken,
+			setToken,
+			startAuthFlow,
+			resetRouter,
 		},
 	};
 };
 
 describe("HomeScreen [Integration]", () => {
 	it("should handle loading state because code is available", async () => {
-		renderWithProviders(
-			<HomeScreen />,
-			diFactory({
-				token: "unauth",
-				code: "code",
-			}),
-		);
+		const { di } = diFactory({
+			token: "unauth",
+			code: "code",
+		});
+
+		renderWithProviders(<HomeScreen />, di);
 
 		expect(screen.getByTestId("lazy-loaded-skeleton")).toBeDefined();
+
 		await waitFor(() => {
 			expect(screen.queryByTestId("home-screen-content")).toBeNull();
 		});
 	});
 
 	it("should request access-token if code is available and reset route", async () => {
-		const requestAccessToken = vi.fn();
-		const resetRouter = vi.fn();
-		const setToken = vi.fn();
-
-		const di = diFactory({
+		const { di, deps } = diFactory({
 			token: "unauth",
 			code: "code",
 		});
 
-		requestAccessToken.mockResolvedValueOnce("code");
-		renderWithProviders(<HomeScreen />, {
-			...di,
-			repositories: {
-				...di?.repositories,
-				auth: {
-					...di?.repositories?.auth,
-					requestAccessToken,
-				},
-			},
-			adapters: {
-				...di?.adapters,
-				authAdapter: {
-					...di?.adapters?.authAdapter,
-					setToken,
-				},
-				routerAdapter: {
-					...di?.adapters?.routerAdapter,
-					reset: resetRouter,
-				},
-			},
-		});
+		deps.requestAccessToken.mockResolvedValueOnce("code");
+
+		renderWithProviders(<HomeScreen />, di);
 
 		await waitFor(() => {
 			const payload: IAuthRepositoryPayload["IRequestAccessTokenIn"] = {
 				code: "code",
 			};
-			expect(requestAccessToken).toHaveBeenCalledWith(payload);
+			expect(deps.requestAccessToken).toHaveBeenCalledWith(payload);
 		});
 
 		await waitFor(() => {
 			const payload: IAuthAdapterPayload["ISetTokenIn"] = {
 				token: "code",
 			};
-			expect(setToken).toHaveBeenCalledWith(payload);
-			expect(resetRouter).toHaveBeenCalled();
+			expect(deps.setToken).toHaveBeenCalledWith(payload);
+			expect(deps.resetRouter).toHaveBeenCalled();
 		});
 	});
 
 	it("should render login button if unauthenticated", async () => {
-		const startAuthFlow = vi.fn();
+		const { di, deps } = diFactory({ token: "unauth" });
 
-		const di = diFactory({ token: "unauth" });
-
-		renderWithProviders(<HomeScreen />, {
-			...di,
-			adapters: {
-				...di?.adapters,
-				authAdapter: {
-					...di?.adapters?.authAdapter,
-					startAuthFlow,
-				},
-			},
-		});
+		renderWithProviders(<HomeScreen />, di);
 
 		await waitFor(() => {
 			expect(screen.getByTestId("home-screen-content")).toBeInTheDocument();
@@ -126,23 +112,13 @@ describe("HomeScreen [Integration]", () => {
 		const button = await screen.findByRole("button", { name: /login/i });
 		await userEvent.click(button);
 
-		await waitFor(() => {
-			expect(startAuthFlow).toHaveBeenCalledTimes(1);
-		});
+		expect(deps.startAuthFlow).toHaveBeenCalledTimes(1);
 	});
 
 	it("should render start-now button if authenticated", async () => {
-		const di = diFactory({ token: "auth" });
+		const { di } = diFactory({ token: "auth" });
 
-		renderWithProviders(<HomeScreen />, {
-			...di,
-			adapters: {
-				...di?.adapters,
-				routerAdapter: {
-					...di?.adapters?.routerAdapter,
-				},
-			},
-		});
+		renderWithProviders(<HomeScreen />, di);
 
 		await waitFor(() => {
 			expect(screen.getByTestId("home-screen-content")).toBeInTheDocument();
@@ -151,6 +127,7 @@ describe("HomeScreen [Integration]", () => {
 		const button = await screen.findByRole<HTMLAnchorElement>("link", {
 			name: /start now/i,
 		});
+
 		expect(new URL(button.href).pathname).toBe(
 			navigationAdapter.generateRoute({
 				name: RouteName.DASHBOARD,
