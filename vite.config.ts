ef09ffabec3d1f3b84ad43a0ceedd7f2a8ce39fa
saacostam <73088662+saacostam@users.noreply.github.com>
2026-02-15 12:50:20 -0500

    chore(build): output to docs folder

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: "./",
+	build: {
+		outDir: "./docs",
+	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
